2308c0c8367bc1be721651940ac0a110812ebb0e
Leon.Xu <xhl@live.it>
2022-05-07 09:45:25 +0800

    feat: UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from 'components/Header';
+import Body from 'components/Body';
 import { title } from 'config';
 import './App.css';
 
 function App() {
+  
   return (
     <div className='App'>
       <Header title={title} />
+      <Body>
+        todo
+      </Body>
     </div>
   );
 }
